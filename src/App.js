94a1badbe6c219a27e8ae18f7e80590708682683
AdityaTarale <tarale.adi@gmail.com>
2021-05-30 19:51:36 +0530

    basic webpack setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const App = () => {
+  return <h1>React Webpack Starter Template !!!!</h1>;
+};
+
+export default App;
---
+++ webpack/webpack.config.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
+  context: path.resolve(__dirname),
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+  mode: "development",
+  devServer: {
+    port: 3000,
+  },
+};
