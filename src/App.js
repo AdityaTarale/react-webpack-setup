c4028e6b810b15e0c3f8ab3ceb9a0242d23bc19b
AdityaTarale <tarale.adi@gmail.com>
2021-05-30 20:29:39 +0530

    added images and svg support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./styles.scss";
-
+import IMAGE from "./react.jpg";
+import Logo from "./logo.svg";
 const App = () => {
-  return <h1>React Webpack Starter Template !!!!</h1>;
+  return (
+    <div className="text-center">
+      <h1>React Webpack Starter Template !!!!</h1>
+      <img src={Logo} alt="React logo" width="400" height="300" />
+    </div>
+  );
 };
 
 export default App;
